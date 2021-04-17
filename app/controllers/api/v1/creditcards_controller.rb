a220a67df3b7a65d454447b32ec94d412c3af7aa
dlashinsky <da.lashinsky@gmail.com>
2021-04-16 17:42:48 -0700

    pushing all changes

--- app/controllers/api/v1/creditcards_controller.rb
+++ app/controllers/api/v1/creditcards_controller.rb
@@ -5,11 +5,25 @@ module Api
             before_action :authenticate 
             
             def index
-                creditcards = CreditCard.where(user_id: @user.id)
-                # render json: { creditcards: creditcards }
-                render json: CreditCardSerializer.new(creditcards).serialized_json
+                
+                if creditcards = CreditCard.where(user_id: @user.id)
+                    render json: CreditCardSerializer.new(creditcards).serialized_json
+                else
+                    render json: { message: "cannot locate #{@user.first_name}'s credit cards. If you haven't added one, please add one now, or reload browser and log back in."}, status: 400
+                end
             end
-        
+            
+            def show 
+
+                if creditcard = CreditCard.find_by(id: params[:id])
+                    render json: CreditCardSerializer.new(creditcard).serialized_json
+                else
+                    render json: { message: "Incorrect value or no such instance exists"}, status: 400
+                end
+            
+            end
+
+
             def create
                 
                 credit_card = CreditCard.create(
@@ -26,7 +40,40 @@ module Api
 
                     # render json: { credit_card: credit_card }
                     render json: CreditCardSerializer.new(credit_card).serialized_json
+            end 
+
+            def destroy
+                credit_card = CreditCard.find_by(id: params[:id])
+                    if !credit_card
+                        render json: { message: "Could not locate and/or delete card." }, status: 400
+                    else
+                        if credit_card.destroy
+                            redirect_to(:action => 'index')
+                        else
+                            render json: { message: "Located Card, but something else went wrong. Sorry, try logging out and in/refresh, try again." }, status: 400
+                        end
+                    end
             end
+
+            def update
+                creditcard = CreditCard.find_by(id: params[:id])
+                    creditcard.update(
+                        nick_name: params[:nick_name],
+                        bank_name: params[:bank_name],
+                        current_bal: params[:current_bal],
+                        credit_limit: params[:credit_limit],
+                        min_payment: params[:min_payment],
+                        actual_payment: params[:actual_payment],
+                        last_four_card: params[:last_four_card],
+                        payment_day: params[:payment_day],
+                        user_id: @user.id
+                        )
+                
+                redirect_to(:action => 'show')
+            end
+
+
+            
         end
     end
 end
